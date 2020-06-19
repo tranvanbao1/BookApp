4449f534239634ae146517ab61291fa1d2b5ec62
Brady Tran <baotrank22tpm4@gmail.com>
2020-06-19 11:34:46 +0700

    fix play/pause in PlayScreen

--- src/components/home/Featured.js
+++ src/components/home/Featured.js
@@ -50,7 +50,221 @@ const data = [
       'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
     duration: 143,
   },
-  
+  {
+    id: 1,
+    name: 'Đắc Nhân Tâm',
+    author: 'Dale Carnegie',
+    imageUrl: require('../../images/dnt.jpg'),
+    des:
+      'Đắc nhân tâm (tên tiếng Anh là How to Win Friends and Influence People) là một quyển sách tự giúp bản thân, do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được dịch ra các thứ tiếng và bán trên khắp thế giới. Quyển sách đưa ra các lời khuyên về cách thức cư xử, ứng xử và giao tiếp với mọi người để đạt được thành công trong cuộc sống. Không còn nữa khái niệm giới hạn Đắc Nhân Tâm là nghệ thuật thu phục lòng người, là làm cho tất cả mọi người yêu mến mình. Đắc nhân tâm và cái Tài trong mỗi người chúng ta. Đắc Nhân Tâm trong ý nghĩa đó cần được thụ đắc bằng sự hiểu rõ bản thân, thành thật với chính mình, hiểu biết và quan tâm đến những người xung quanh để nhìn ra và khơi gợi những tiềm năng ẩn khuất nơi họ, giúp họ phát triển lên một tầm cao mới. Đây chính là nghệ thuật cao nhất về con người và chính là ý nghĩa sâu sắc nhất đúc kết từ những nguyên tắc vàng của Dale Carnegie. Quyển sách đã từng mang đến thành công và hạnh phúc cho hàng triệu người trên khắp thế giới. Và bây giờ các bạn và tôi cùng bắt đầu phân tích và tìm hiểu nó !',
+    chapters: {
+      chapter1: {
+        chapterId: 1,
+        chapterName: 'Đắc Nhân Tâm - Phần 1',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter2: {
+        chapterId: 2,
+        chapterName: 'Đắc Nhân Tâm - Phần 2',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter3: {
+        chapterId: 3,
+        chapterName: 'Đắc Nhân Tâm - Phần 3',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter4: {
+        chapterId: 4,
+        chapterName: 'Đắc Nhân Tâm - Phần 4',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter5: {
+        chapterId: 5,
+        chapterName: 'Đắc Nhân Tâm - Phần 5',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+    },
+    url:
+      'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+    duration: 143,
+  },
+  {
+    id: 1,
+    name: 'Đắc Nhân Tâm',
+    author: 'Dale Carnegie',
+    imageUrl: require('../../images/dnt.jpg'),
+    des:
+      'Đắc nhân tâm (tên tiếng Anh là How to Win Friends and Influence People) là một quyển sách tự giúp bản thân, do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được dịch ra các thứ tiếng và bán trên khắp thế giới. Quyển sách đưa ra các lời khuyên về cách thức cư xử, ứng xử và giao tiếp với mọi người để đạt được thành công trong cuộc sống. Không còn nữa khái niệm giới hạn Đắc Nhân Tâm là nghệ thuật thu phục lòng người, là làm cho tất cả mọi người yêu mến mình. Đắc nhân tâm và cái Tài trong mỗi người chúng ta. Đắc Nhân Tâm trong ý nghĩa đó cần được thụ đắc bằng sự hiểu rõ bản thân, thành thật với chính mình, hiểu biết và quan tâm đến những người xung quanh để nhìn ra và khơi gợi những tiềm năng ẩn khuất nơi họ, giúp họ phát triển lên một tầm cao mới. Đây chính là nghệ thuật cao nhất về con người và chính là ý nghĩa sâu sắc nhất đúc kết từ những nguyên tắc vàng của Dale Carnegie. Quyển sách đã từng mang đến thành công và hạnh phúc cho hàng triệu người trên khắp thế giới. Và bây giờ các bạn và tôi cùng bắt đầu phân tích và tìm hiểu nó !',
+    chapters: {
+      chapter1: {
+        chapterId: 1,
+        chapterName: 'Đắc Nhân Tâm - Phần 1',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter2: {
+        chapterId: 2,
+        chapterName: 'Đắc Nhân Tâm - Phần 2',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter3: {
+        chapterId: 3,
+        chapterName: 'Đắc Nhân Tâm - Phần 3',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter4: {
+        chapterId: 4,
+        chapterName: 'Đắc Nhân Tâm - Phần 4',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter5: {
+        chapterId: 5,
+        chapterName: 'Đắc Nhân Tâm - Phần 5',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+    },
+    url:
+      'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+    duration: 143,
+  },
+  {
+    id: 1,
+    name: 'Đắc Nhân Tâm',
+    author: 'Dale Carnegie',
+    imageUrl: require('../../images/dnt.jpg'),
+    des:
+      'Đắc nhân tâm (tên tiếng Anh là How to Win Friends and Influence People) là một quyển sách tự giúp bản thân, do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được dịch ra các thứ tiếng và bán trên khắp thế giới. Quyển sách đưa ra các lời khuyên về cách thức cư xử, ứng xử và giao tiếp với mọi người để đạt được thành công trong cuộc sống. Không còn nữa khái niệm giới hạn Đắc Nhân Tâm là nghệ thuật thu phục lòng người, là làm cho tất cả mọi người yêu mến mình. Đắc nhân tâm và cái Tài trong mỗi người chúng ta. Đắc Nhân Tâm trong ý nghĩa đó cần được thụ đắc bằng sự hiểu rõ bản thân, thành thật với chính mình, hiểu biết và quan tâm đến những người xung quanh để nhìn ra và khơi gợi những tiềm năng ẩn khuất nơi họ, giúp họ phát triển lên một tầm cao mới. Đây chính là nghệ thuật cao nhất về con người và chính là ý nghĩa sâu sắc nhất đúc kết từ những nguyên tắc vàng của Dale Carnegie. Quyển sách đã từng mang đến thành công và hạnh phúc cho hàng triệu người trên khắp thế giới. Và bây giờ các bạn và tôi cùng bắt đầu phân tích và tìm hiểu nó !',
+    chapters: {
+      chapter1: {
+        chapterId: 1,
+        chapterName: 'Đắc Nhân Tâm - Phần 1',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter2: {
+        chapterId: 2,
+        chapterName: 'Đắc Nhân Tâm - Phần 2',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter3: {
+        chapterId: 3,
+        chapterName: 'Đắc Nhân Tâm - Phần 3',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter4: {
+        chapterId: 4,
+        chapterName: 'Đắc Nhân Tâm - Phần 4',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter5: {
+        chapterId: 5,
+        chapterName: 'Đắc Nhân Tâm - Phần 5',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+    },
+    url:
+      'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+    duration: 143,
+  },
+  {
+    id: 1,
+    name: 'Đắc Nhân Tâm',
+    author: 'Dale Carnegie',
+    imageUrl: require('../../images/dnt.jpg'),
+    des:
+      'Đắc nhân tâm (tên tiếng Anh là How to Win Friends and Influence People) là một quyển sách tự giúp bản thân, do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được dịch ra các thứ tiếng và bán trên khắp thế giới. Quyển sách đưa ra các lời khuyên về cách thức cư xử, ứng xử và giao tiếp với mọi người để đạt được thành công trong cuộc sống. Không còn nữa khái niệm giới hạn Đắc Nhân Tâm là nghệ thuật thu phục lòng người, là làm cho tất cả mọi người yêu mến mình. Đắc nhân tâm và cái Tài trong mỗi người chúng ta. Đắc Nhân Tâm trong ý nghĩa đó cần được thụ đắc bằng sự hiểu rõ bản thân, thành thật với chính mình, hiểu biết và quan tâm đến những người xung quanh để nhìn ra và khơi gợi những tiềm năng ẩn khuất nơi họ, giúp họ phát triển lên một tầm cao mới. Đây chính là nghệ thuật cao nhất về con người và chính là ý nghĩa sâu sắc nhất đúc kết từ những nguyên tắc vàng của Dale Carnegie. Quyển sách đã từng mang đến thành công và hạnh phúc cho hàng triệu người trên khắp thế giới. Và bây giờ các bạn và tôi cùng bắt đầu phân tích và tìm hiểu nó !',
+    chapters: {
+      chapter1: {
+        chapterId: 1,
+        chapterName: 'Đắc Nhân Tâm - Phần 1',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter2: {
+        chapterId: 2,
+        chapterName: 'Đắc Nhân Tâm - Phần 2',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter3: {
+        chapterId: 3,
+        chapterName: 'Đắc Nhân Tâm - Phần 3',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter4: {
+        chapterId: 4,
+        chapterName: 'Đắc Nhân Tâm - Phần 4',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter5: {
+        chapterId: 5,
+        chapterName: 'Đắc Nhân Tâm - Phần 5',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+    },
+    url:
+      'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+    duration: 143,
+  },
+  {
+    id: 1,
+    name: 'Đắc Nhân Tâm',
+    author: 'Dale Carnegie',
+    imageUrl: require('../../images/dnt.jpg'),
+    des:
+      'Đắc nhân tâm (tên tiếng Anh là How to Win Friends and Influence People) là một quyển sách tự giúp bản thân, do Dale Carnegie viết và đã được xuất bản lần đầu vào năm 1936, nó đã được dịch ra các thứ tiếng và bán trên khắp thế giới. Quyển sách đưa ra các lời khuyên về cách thức cư xử, ứng xử và giao tiếp với mọi người để đạt được thành công trong cuộc sống. Không còn nữa khái niệm giới hạn Đắc Nhân Tâm là nghệ thuật thu phục lòng người, là làm cho tất cả mọi người yêu mến mình. Đắc nhân tâm và cái Tài trong mỗi người chúng ta. Đắc Nhân Tâm trong ý nghĩa đó cần được thụ đắc bằng sự hiểu rõ bản thân, thành thật với chính mình, hiểu biết và quan tâm đến những người xung quanh để nhìn ra và khơi gợi những tiềm năng ẩn khuất nơi họ, giúp họ phát triển lên một tầm cao mới. Đây chính là nghệ thuật cao nhất về con người và chính là ý nghĩa sâu sắc nhất đúc kết từ những nguyên tắc vàng của Dale Carnegie. Quyển sách đã từng mang đến thành công và hạnh phúc cho hàng triệu người trên khắp thế giới. Và bây giờ các bạn và tôi cùng bắt đầu phân tích và tìm hiểu nó !',
+    chapters: {
+      chapter1: {
+        chapterId: 1,
+        chapterName: 'Đắc Nhân Tâm - Phần 1',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter2: {
+        chapterId: 2,
+        chapterName: 'Đắc Nhân Tâm - Phần 2',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter3: {
+        chapterId: 3,
+        chapterName: 'Đắc Nhân Tâm - Phần 3',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter4: {
+        chapterId: 4,
+        chapterName: 'Đắc Nhân Tâm - Phần 4',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+      chapter5: {
+        chapterId: 5,
+        chapterName: 'Đắc Nhân Tâm - Phần 5',
+        audioUrl:
+          'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+      },
+    },
+    url:
+      'https://drive.google.com/uc?export=download&id=1VM9_umeyzJn0v1pRzR1BSm9y3IhZ3c0E',
+    duration: 143,
+  },
 ];
 
 class Featured extends React.Component {
--- src/pages/PlayScreen.js
+++ src/pages/PlayScreen.js
@@ -42,11 +42,12 @@ export default function PlaylistScreen({route}) {
         url: chapter.audioUrl,
         name: chapter.chapterName,
         author: playlistData.author,
-        imageUrl: require('../images/dnt.jpg'),
+        imageUrl: 'https://images.squarespace-cdn.com/content/v1/5a5906400abd0406785519dd/1552662149940-G6MMFW3JC2J61UBPROJ5/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/baelen.jpg?format=1500w',
       });    
       TrackPlayer.play();
     } else {
       if (playbackState === TrackPlayer.STATE_PAUSED) {
+        await TrackPlayer.play();
       } else {
         await TrackPlayer.pause();
       }
